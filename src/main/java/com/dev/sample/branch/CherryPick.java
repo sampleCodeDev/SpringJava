eadea2b5b32407c662984bbe537cec8acbb80151
devdevdev <heodaewon92@kakao.com>
2022-04-11 23:33:27 +0900

    cherry 2222

--- src/main/java/com/dev/sample/branch/CherryPick.java
+++ src/main/java/com/dev/sample/branch/CherryPick.java
@@ -1,7 +1,12 @@
 package com.dev.sample.branch;
 
 public class CherryPick {
-    private void Cherry1(){
+    private void cherry1(){
         System.out.println("cherry-pick");
     }
+
+    private void cherry2(){
+        System.out.println("cherry-pick 2222");
+    }
+
 }
